e93fdec42c69f2d45691d993572d0f5e1e88efd3
Herles Incalla <herles.incalla@gmail.com>
2018-08-08 19:44:32 -0500

    Improve right theme

--- static/src/js/app.js
+++ static/src/js/app.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
 	// $('div.wrapper').addClass('scrollable scrollbar-macosx').scrollbar({'disableBodyScroll' : true});
-	$('div.o_content').addClass('scrollable scrollbar-macosx').scrollbar({'disableBodyScroll' : true});
+	$('div.wrapper').addClass('scrollable scrollbar-macosx').scrollbar({'disableBodyScroll' : true});
+
+    $(window).on("scroll", function () {
+        if ($(this).scrollTop() !== 0) {
+            $('#toTop').fadeIn();
+        } else {
+            // $('#toTop').fadeOut();
+        }
+    });
+    $('#toTop').on("click", function () {
+        // $("html, body").animate({scrollTop: 0}, 600);
+		$('html, body').animate({
+			scrollTop: $(".wrapper").offset().top
+		}, 600);
+		
+		return false;
+    });
+
 });
